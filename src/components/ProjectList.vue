fcc55aa8c579ca87fc2e4a926a8b6cc01979b050
Ailzac <a22647814.jl@gmail.com>
2020-12-30 13:39:42 +0800

    修改

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,21 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <ul v-for="(item, id) in projects" :key="id">
+      <li>
+        <a :href="item.to">{{ item.title }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
 import vue from "vue";
 import { root } from "../scripts/common/config";
 export default vue.extend({
-  data: {
-    projects: [
-      {
-        title: "galaxy",
-        description: "",
-        tags: ["playcanvas", "webgl"],
-        preview: root + "projects/preview.jpg",
-        to: root + "projects/galaxy",
-      },
-    ],
+  data: function () {
+    return {
+      projects: [
+        {
+          title: "galaxy",
+          description: "",
+          tags: ["playcanvas", "webgl"],
+          preview: root + "projects/preview.jpg",
+          to: root + "projects/galaxy",
+        },
+      ],
+    };
   },
 });
 </script>
